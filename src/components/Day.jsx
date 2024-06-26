97a1a986993948471ba8151ef19134850c94b381
ZL <ray040406@gmail.com>
2024-06-26 17:29:19 +1200

    add and delete

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,4 +1,44 @@
 import React, { useState } from "react";
 export default function Day() {
-  return <div>x</div>;
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {}
+
+  function deleteTask(index) {}
+
+  function moveTaskUp(index) {}
+
+  function moveTaskDown(index) {}
+
+  return (
+    <div className="to-do-list">
+      <h1>Gym Plan</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="enter an exercise"
+          value={newTask}
+          onChange={handleInputChange}
+        ></input>
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
