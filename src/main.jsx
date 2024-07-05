93a52c9b299b427133c79b06f51493443b8088f1
ZL <ray040406@gmail.com>
2024-07-05 20:38:47 +1200

    revert

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
 
-const basename = "/hosting-template";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename={basename}>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
